b60087f8bcaa15cd30b71e3bf6ae0cd61bc5ccca
Las02 <97394529+Las02@users.noreply.github.com>
2023-08-14 18:19:15 +0200

    Update remove_snp.py

--- AntibioticResistance/remove_snp.py
+++ AntibioticResistance/remove_snp.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import glob
-files = glob.glob("../dataMEGARES3/*")
 
+# Read in the data about which antibiotic resistance genes come form a SNP
 snp = pd.read_csv("isSNP.csv")
 snp = snp["V1"].values
 snp = [">"+x for x in snp]
 
+# Go through each Abricate output and filter out each SNP if they do not have 100% coverage and 100% identity
+files = glob.glob("../dataMEGARES3/*")
 for path in files:
     file =  open(path, "r")
     file = file.readlines()
@@ -15,9 +17,9 @@ for path in files:
     print("lines before", len(file))
     i=0
     for line in file:
-        
         line = line.strip()
         line_split=line.split("\t")
+        # Check if they have 100% coverage and 100% identity
         if line_split[4] in snp and line_split[8] != "100.00" and line_split[9] != "100.00":
             continue
         else:
